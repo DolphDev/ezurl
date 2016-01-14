1e25dc165cc996d87c07bb10db7f6921cdba9ee8
Dolphman <therealdolphman@gmail.com>
2016-01-14 02:26:01 -0500

    Added Tests

--- tests/test_url.py
+++ tests/test_url.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import OrderedDict
 from ezurl import Url
 
 
@@ -30,23 +31,50 @@ class url_correct_test(unittest.TestCase):
         test_url.page("WORLD")
         self.assertEqual(str(test_url), "https://myurl.com/HELLO/WORLD")
 
+    def test_url_querytrack_multi_query(self):
+        test_url = Url("myurl.com").query(a="TEST", b="TEST")
+        self.assertEqual(dict(test_url.querytrack), {"a":"TEST", "b":"TEST"})
+
+
 class url_generation_test_type(unittest.TestCase):
 
-    def test_query_gen(self):
+    def test_query_gen_type(self):
         test_url = Url("myurl.com").query(a=[0,1])
         self.assertIsInstance(test_url._query_gen(), str)
 
-    def test_page_gen(self):
+    def test_page_gen_type(self):
         test_url = Url("myurl.com").page("HELLO")
         self.assertIsInstance(test_url._page_gen(), str)
 
-def url_generation_equalto(self):
+class url_generation_equalto(unittest.TestCase):
 
-    def test_query_gen(self):
+    def test_query_gen_equalto(self):
         test_url = Url("myurl.com").query(a=[0,1])
         self.assertEqual(test_url._query_gen(), "?a=0+1")
 
-    def test_page_gen(self):
+    def test_querytrack_equalto(self):
+        test_url = Url("myurl.com").query(a=[0,1])
+        self.assertEqual(test_url.querytrack, OrderedDict({"a":"0+1"}))
+
+    def test_page_gen_equalto(self):
         test_url = Url("myurl.com").page("HELLO")
-        self.assertEqual(test_url._page_gen(), ["HELLO"])
-        
+        self.assertEqual(test_url._page_gen(), "/HELLO")
+
+    def test_page_equalto(self):
+        test_url = Url("myurl.com").page("HELLO")
+        self.assertEqual(test_url.pagestrack, ["HELLO"])
+
+class url_attribute_type_test(unittest.TestCase):
+
+    def test_querytrack_type(self):
+        test_url = Url("")
+        self.assertIsInstance(test_url.querytrack, OrderedDict)
+
+    def test_pagetrack_type(self):
+        test_url = Url("")
+        self.assertIsInstance(test_url.pagestrack, list)
+
+    def test_fragement_type(self):
+        test_url = Url("")
+        self.assertIsInstance(test_url.fragmenttrack, str)
+

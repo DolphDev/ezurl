1b386a06d13b8e551d1bf3a8cd8055323effc6a2
Dolphman <therealdolphman@gmail.com>
2016-02-02 09:25:52 -0500

    0.1.3.22 release

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ SOFTWARE.
 
 from setuptools import setup
 setup(name='ezurl',
-      version="0.1.2.20",
+      version="0.1.3.22",
       description='URL Generation for Python',
       author='Joshua Walters',
       author_email='therealdolphman@gmail.com',

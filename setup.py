97fc1422198c7d56ed9db76578b8d84554c99505
Dolphman <therealdolphman@gmail.com>
2016-02-16 15:06:34 -0500

    feature fix release

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ SOFTWARE.
 
 from setuptools import setup
 setup(name='ezurl',
-      version="0.1.3.23",
+      version="0.1.3.24",
       description='URL Generation for Python',
       author='Joshua Walters',
       author_email='therealdolphman@gmail.com',
